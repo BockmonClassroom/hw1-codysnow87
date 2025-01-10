1e4769e8d1b053b5b54ba9bd086dcbd402670091
codysnow87 <snow.cod@northeastern.edu>
2025-01-09 19:45:04 -0500

    Wrote code for HW1 to read in and iterate through iris data

--- HW1/HW1.py
+++ HW1/HW1.py
@@ -1,1 +1,17 @@
-import 
+# Cody Snow 
+# HW1 DS5110
+# January 9th, 2025
+
+# Import the pandas library and name it pd
+import pandas as pd
+
+# Read the Iris.csv file into a pandas dataframe
+df = pd.read_csv('./data/Iris.csv')
+
+# Print the first 5 rows of the data to ensure it was read correctly
+print("The first 5 rows of the data are:")
+print(df.head())
+
+# Print the entire data frame row by row using the itertuples() function and a foreach loop
+for row in df.itertuples():
+    print(row)
